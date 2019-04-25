2728528f08eb943ba82c2276e17ee245601150d0
Antoine Cajot <antoine.cajot@student.uliege.be>
2019-04-25 13:30:02 +0200

    Preprocessing now also pushes properly formatted gender and age data into indexes for easier loading

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import numpy as np
 import math
+import re
 
 target_size = 500
 
@@ -69,55 +70,84 @@ def preprocess_data():
         next(index)  # Skip header row
         entries.extend(index.readlines())
 
-    data_index = []  # Format : list of [`image path in db`, `age`, `gender`]
+    image_index = []
+    gender_index = []
+    age_index = []  # Format : (`mean`, `standard_deviation`)
 
-    # Resize them to fixed size + filling data_index
+    # Resize them to fixed size + filling data indexes
     for i in range(0, len(entries)):
         print_progress_bar(i + 1, len(entries), 'Resizing images : ')
         entry = entries[i]
         entry = entry.split("\t")
         save_path = resize_image(entry[0] + "/" + "coarse_tilt_aligned_face." + entry[2] + "." + entry[1])
-        data_index.append([save_path, entry[3], entry[4]])
+        image_index.append(save_path)
+        try:
+            if entry[4] == "f" or entry[4] == "m":
+                gender_index.append(1 if entry[4] == "f" else 0)
+            else:
+                raise ValueError
+            if entry[3][0] == '(':
+                interval = re.sub("\(|\)|\s", "", entry[3]).split(",")
+                bottom = int(interval[0])
+                top = int(interval[1])
+                age_index.append(((bottom + top) / 2, (top - bottom) / 2))
+            else:
+                age_index.append((int(entry[3]), 0))
+        except ValueError:
+            image_index.pop()
+
+    # Flushing indexes to disk
+    with open('db/preprocessed/image.index', 'w+') as f_handle:
+        for item in image_index:
+            f_handle.write('%s\n' % item)
+
+    with open('db/preprocessed/age.index', 'w+') as f_handle:
+        for item in age_index:
+            f_handle.write('%s %s\n' % item)
+
+    with open('db/preprocessed/gender.index', 'w+') as f_handle:
+        for item in gender_index:
+            f_handle.write('%s\n' % item)
 
     # Work out mean image
     mean_img = np.zeros((target_size, target_size, 3), np.uint32)
-    for i in range(0, len(data_index)):
-        print_progress_bar(i + 1, len(data_index), 'Working out mean : ')
-        img = cv2.imread(data_index[i][0])
+    for i in range(0, len(image_index)):
+        print_progress_bar(i + 1, len(image_index), 'Working out mean : ')
+        img = cv2.imread(image_index[i])
         mean_img += img
-    mean_img = np.true_divide(mean_img, len(data_index))
+    mean_img = np.true_divide(mean_img, len(image_index))
     cv2.imwrite("db/preprocessed/mean.jpg", mean_img)
 
     # Work out standard deviation image
     standard_deviation_img = np.zeros((target_size, target_size, 3), np.uint32)
-    for i in range(0, len(data_index)):
-        print_progress_bar(i + 1, len(data_index), 'Working out standard deviation : ')
-        img = cv2.imread(data_index[i][0])
+    for i in range(0, len(image_index)):
+        print_progress_bar(i + 1, len(image_index), 'Working out standard deviation : ')
+        img = cv2.imread(image_index[i])
         standard_deviation_img += np.power(img - mean_img, 2).astype(np.uint32)
-    standard_deviation_img = np.sqrt(np.true_divide(standard_deviation_img, len(data_index) - 1))
+    standard_deviation_img = np.sqrt(np.true_divide(standard_deviation_img, len(image_index) - 1))
     cv2.imwrite("db/preprocessed/standard_deviation.jpg", standard_deviation_img)
 
     # Normalizing images
     absolute_min = math.inf
     absolute_max = -math.inf
-    for i in range(0, len(data_index)):
-        print_progress_bar(i + 1, len(data_index), 'Retrieving image scale : ')
-        img = ((cv2.imread(data_index[i][0]).astype(np.int16) - mean_img) / standard_deviation_img)
+    for i in range(0, len(image_index)):
+        print_progress_bar(i + 1, len(image_index), 'Retrieving image scale : ')
+        img = ((cv2.imread(image_index[i]).astype(np.int16) - mean_img) / standard_deviation_img)
         img_min = np.amin(img)
         if img_min < absolute_min:
             absolute_min = img_min
         img_max = np.amax(img)
         if img_max > absolute_max:
             absolute_max = img_max
-        np.save(data_index[i][0] + ".npy", img.astype(np.int16))
+        np.save(image_index[i] + ".npy", img.astype(np.int16))
 
-    for i in range(0, len(data_index)):
-        print_progress_bar(i + 1, len(data_index), 'Normalizing images : ')
-        img = np.load(data_index[i][0] + ".npy").astype(np.float64)
+    for i in range(0, len(image_index)):
+        print_progress_bar(i + 1, len(image_index), 'Normalizing images : ')
+        img = np.load(image_index[i] + ".npy").astype(np.float64)
         img -= absolute_min  # Start @ 0
         img *= 255 / (absolute_max - absolute_min)  # Occupy full scale
-        cv2.imwrite(data_index[i][0], img)
-        os.remove(data_index[i][0] + ".npy")
+        cv2.imwrite(image_index[i], img)
+        os.remove(image_index[i] + ".npy")
 
     print("All done !")
 
