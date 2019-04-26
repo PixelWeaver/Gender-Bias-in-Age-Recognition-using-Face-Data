99be4b938b6e7c18ae18d96acf5cc42de6e5b832
PixelWeaver <antoine.cajot@student.uliege.be>
2019-04-26 15:05:10 +0200

    Forked utilities into separated files. Created Dataset and NeuralNetwork classes.

--- dataset.py
+++ dataset.py
@@ -3,157 +3,181 @@ import os
 import numpy as np
 import math
 import re
-
-target_size = 500
-
-
-# Print iterations progress
-# FROM https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
-def print_progress_bar(iteration, total, prefix='Progress :', suffix='Complete', decimals=1, length=50, fill='█'):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filled_length = int(length * iteration // total)
-    bar = fill * filled_length + '-' * (length - filled_length)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
-    # Print New Line on Complete
-    if iteration == total:
-        print()
+import util
 
 
-def resize_image(img_path):
-    img = cv2.imread("db/faces/" + img_path)
-    old_dimensions = img.shape[:2]
+class Dataset:
+    def __init__(self, force_preprocessing=False):
+        self.db_path = "db/"
+        self.unprocessed_path = "faces/"
+        self.preprocessed_path = "preprocessed/"
+        self.image_index = []
+        self.age_index = []
+        self.gender_index = []
+        self.target_image_size = 500
 
-    ratio = float(target_size) / max(old_dimensions)
-    new_dimensions = tuple([int(x * ratio) for x in old_dimensions])
+        if not os.path.isfile(self.db_path + self.preprocessed_path + "lock") or force_preprocessing:
+            print("Started preprocessing data...")
+            self.preprocess_data()
 
-    img = cv2.resize(img, (new_dimensions[1], new_dimensions[0]))
+        self.load_indexes()
 
-    delta_w = target_size - new_dimensions[1]
-    delta_h = target_size - new_dimensions[0]
-    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
-    left, right = delta_w // 2, delta_w - (delta_w // 2)
+    def load_indexes(self):
+        with open('db/preprocessed/image.index', 'r') as f_handle:
+            formatted_image_index = f_handle.readlines()
+            for formatted_image_input in formatted_image_index:
+                self.image_index.append(formatted_image_input[:-1])
 
-    color = [0, 0, 0]
-    new_img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                 value=color)
+        with open('db/preprocessed/age.index', 'r') as f_handle:
+            formatted_age_index = f_handle.readlines()
+            for formatted_age_input in formatted_age_index:
+                values = formatted_age_input[:-1]
+                values = values.split(" ")
 
-    # Ensure directory exists or is created !
-    save_path = "db/preprocessed/" + img_path.replace("/", "_")
-    directory = os.path.dirname(save_path)
-    try:
-        os.makedirs(directory)
-    except FileExistsError:
-        pass  # It's okay, nothing to be done
+                self.age_index.append((float(values[0]), float(values[1])))
 
-    cv2.imwrite(save_path, new_img)
+        with open('db/preprocessed/gender.index', 'r') as f_handle:
+            formatted_gender_index = f_handle.readlines()
+            for formatted_gender_input in formatted_gender_index:
+                self.gender_index.append(int(formatted_gender_input[:-1]))
 
-    return save_path
+        train_count = 500
+        test_count = 50
 
+        x_train = []
+        y_train = []
+        x_test = []
+        y_test = []
 
-def preprocess_data():
-    # Retrieve every image entry in the database
-    data_indexes = ["fold_0_data.txt", "fold_1_data.txt", "fold_2_data.txt", "fold_3_data.txt", "fold_4_data.txt"]
-    entries = []
-    for i in range(0, len(data_indexes)):
-        index = open("db/" + data_indexes[i], 'r')
-        next(index)  # Skip header row
-        entries.extend(index.readlines())
+        for i in range(0, train_count):
+            x_train.append(cv2.imread(self.image_index[i]))
+            y_train.append(self.age_index[i])
 
-    image_index = []
-    gender_index = []
-    age_index = []  # Format : (`mean`, `standard_deviation`)
+        for i in range(train_count, test_count):
+            x_test.append(cv2.imread(self.image_index[i]))
+            y_test.append(self.age_index[i])
 
-    # Resize them to fixed size + filling data indexes
-    for i in range(0, len(entries)):
-        print_progress_bar(i + 1, len(entries), 'Resizing images : ')
-        entry = entries[i]
-        entry = entry.split("\t")
-        save_path = resize_image(entry[0] + "/" + "coarse_tilt_aligned_face." + entry[2] + "." + entry[1])
-        image_index.append(save_path)
+        return (x_train, y_train), (x_test, y_test)
+
+    def resize_image(self, img_path_in_db):
+        img = cv2.imread(self.db_path + self.unprocessed_path + img_path_in_db)
+        old_dimensions = img.shape[:2]
+
+        ratio = float(self.target_image_size) / max(old_dimensions)
+        new_dimensions = tuple([int(x * ratio) for x in old_dimensions])
+
+        img = cv2.resize(img, (new_dimensions[1], new_dimensions[0]))
+
+        delta_w = self.target_image_size - new_dimensions[1]
+        delta_h = self.target_image_size - new_dimensions[0]
+        top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+        left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+        color = [0, 0, 0]
+        new_img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT,
+                                     value=color)
+
+        # Ensure directory exists or is created !
+        save_path = self.db_path + self.preprocessed_path + img_path_in_db.replace("/", "_")
+        directory = os.path.dirname(save_path)
         try:
-            if entry[4] == "f" or entry[4] == "m":
-                gender_index.append(1 if entry[4] == "f" else 0)
-            else:
-                raise ValueError
-            if entry[3][0] == '(':
-                interval = re.sub("\(|\)|\s", "", entry[3]).split(",")
-                bottom = int(interval[0])
-                top = int(interval[1])
-                age_index.append(((bottom + top) / 2, (top - bottom) / 2))
-            else:
-                age_index.append((int(entry[3]), 0))
-        except ValueError:  # ignore [incorrectly formatted / incomplete] lines
-            image_index.pop()
+            os.makedirs(directory)
+        except FileExistsError:
+            pass  # It's okay, nothing to be done
 
-    # Flushing indexes to disk
-    with open('db/preprocessed/image.index', 'w+') as f_handle:
-        for item in image_index:
-            f_handle.write('%s\n' % item)
+        cv2.imwrite(save_path, new_img)
 
-    with open('db/preprocessed/age.index', 'w+') as f_handle:
-        for item in age_index:
-            f_handle.write('%s %s\n' % item)
+        return save_path
 
-    with open('db/preprocessed/gender.index', 'w+') as f_handle:
-        for item in gender_index:
-            f_handle.write('%s\n' % item)
+    def preprocess_data(self):
+        # Retrieve every image entry in the database
+        data_indexes = ["fold_0_data.txt", "fold_1_data.txt", "fold_2_data.txt", "fold_3_data.txt", "fold_4_data.txt"]
+        entries = []
+        for i in range(0, len(data_indexes)):
+            index = open("db/" + data_indexes[i], 'r')
+            next(index)  # Skip header row
+            entries.extend(index.readlines())
 
-    # Work out mean image
-    mean_img = np.zeros((target_size, target_size, 3), np.uint32)
-    for i in range(0, len(image_index)):
-        print_progress_bar(i + 1, len(image_index), 'Working out mean : ')
-        img = cv2.imread(image_index[i])
-        mean_img += img
-    mean_img = np.true_divide(mean_img, len(image_index))
-    cv2.imwrite("db/preprocessed/mean.jpg", mean_img)
+        image_index = []
+        gender_index = []
+        age_index = []  # Format : (`mean`, `standard_deviation`)
 
-    # Work out standard deviation image
-    standard_deviation_img = np.zeros((target_size, target_size, 3), np.uint32)
-    for i in range(0, len(image_index)):
-        print_progress_bar(i + 1, len(image_index), 'Working out standard deviation : ')
-        img = cv2.imread(image_index[i])
-        standard_deviation_img += np.power(img - mean_img, 2).astype(np.uint32)
-    standard_deviation_img = np.sqrt(np.true_divide(standard_deviation_img, len(image_index) - 1))
-    cv2.imwrite("db/preprocessed/standard_deviation.jpg", standard_deviation_img)
+        # Resize them to fixed size + filling data indexes
+        for i in range(0, len(entries)):
+            util.print_progress_bar(i + 1, len(entries), 'Resizing images : ')
+            entry = entries[i]
+            entry = entry.split("\t")
+            save_path = self.resize_image(entry[0] + "/" + "coarse_tilt_aligned_face." + entry[2] + "." + entry[1])
+            image_index.append(save_path)
+            try:
+                if entry[4] == "f" or entry[4] == "m":
+                    gender_index.append(1 if entry[4] == "f" else 0)
+                else:
+                    raise ValueError
+                if entry[3][0] == '(':
+                    interval = re.sub("\(|\)|\s", "", entry[3]).split(",")
+                    bottom = int(interval[0])
+                    top = int(interval[1])
+                    age_index.append(((bottom + top) / 2, (top - bottom) / 2))
+                else:
+                    age_index.append((int(entry[3]), 0))
+            except ValueError:  # ignore [incorrectly formatted / incomplete] lines
+                image_index.pop()
 
-    # Normalizing images
-    absolute_min = math.inf
-    absolute_max = -math.inf
-    for i in range(0, len(image_index)):
-        print_progress_bar(i + 1, len(image_index), 'Retrieving image scale : ')
-        img = ((cv2.imread(image_index[i]).astype(np.int16) - mean_img) / standard_deviation_img)
-        img_min = np.amin(img)
-        if img_min < absolute_min:
-            absolute_min = img_min
-        img_max = np.amax(img)
-        if img_max > absolute_max:
-            absolute_max = img_max
-        np.save(image_index[i] + ".npy", img.astype(np.int16))
+        # Flushing indexes to disk
+        with open('db/preprocessed/image.index', 'w+') as f_handle:
+            for item in image_index:
+                f_handle.write('%s\n' % item)
 
-    for i in range(0, len(image_index)):
-        print_progress_bar(i + 1, len(image_index), 'Normalizing images : ')
-        img = np.load(image_index[i] + ".npy").astype(np.float64)
-        img -= absolute_min  # Start @ 0
-        img *= 255 / (absolute_max - absolute_min)  # Occupy full scale
-        cv2.imwrite(image_index[i], img)
-        os.remove(image_index[i] + ".npy")
+        with open('db/preprocessed/age.index', 'w+') as f_handle:
+            for item in age_index:
+                f_handle.write('%s %s\n' % item)
 
-    print("All done !")
+        with open('db/preprocessed/gender.index', 'w+') as f_handle:
+            for item in gender_index:
+                f_handle.write('%s\n' % item)
 
+        # Work out mean image
+        mean_img = np.zeros((self.target_image_size, self.target_image_size, 3), np.uint32)
+        for i in range(0, len(image_index)):
+            util.print_progress_bar(i + 1, len(image_index), 'Working out mean : ')
+            img = cv2.imread(image_index[i])
+            mean_img += img
+        mean_img = np.true_divide(mean_img, len(image_index))
+        cv2.imwrite("db/preprocessed/mean.jpg", mean_img)
 
-if os.path.isdir("db/preprocessed"):
-    print("Nothing to be done.")
-else:
-    print("Started preprocessing data...")
-    preprocess_data()
+        # Work out standard deviation image
+        standard_deviation_img = np.zeros((self.target_image_size, self.target_image_size, 3), np.uint32)
+        for i in range(0, len(image_index)):
+            util.print_progress_bar(i + 1, len(image_index), 'Working out standard deviation : ')
+            img = cv2.imread(image_index[i])
+            standard_deviation_img += np.power(img - mean_img, 2).astype(np.uint32)
+        standard_deviation_img = np.sqrt(np.true_divide(standard_deviation_img, len(image_index) - 1))
+        cv2.imwrite("db/preprocessed/standard_deviation.jpg", standard_deviation_img)
+
+        # Normalizing images
+        absolute_min = math.inf
+        absolute_max = -math.inf
+        for i in range(0, len(image_index)):
+            util.print_progress_bar(i + 1, len(image_index), 'Retrieving image scale : ')
+            img = ((cv2.imread(image_index[i]).astype(np.int16) - mean_img) / standard_deviation_img)
+            img_min = np.amin(img)
+            if img_min < absolute_min:
+                absolute_min = img_min
+            img_max = np.amax(img)
+            if img_max > absolute_max:
+                absolute_max = img_max
+            np.save(image_index[i] + ".npy", img.astype(np.int16))
+
+        for i in range(0, len(image_index)):
+            util.print_progress_bar(i + 1, len(image_index), 'Normalizing images : ')
+            img = np.load(image_index[i] + ".npy").astype(np.float64)
+            img -= absolute_min  # Start @ 0
+            img *= 255 / (absolute_max - absolute_min)  # Occupy full scale
+            cv2.imwrite(image_index[i], img)
+            os.remove(image_index[i] + ".npy")
+
+        f_handle = open(self.db_path + self.preprocessed_path + "lock", "w+")
+        f_handle.close()
+        print("All done.")
+
--- network.py
+++ network.py
@@ -1,106 +1,69 @@
 import cv2
 import tensorflow as tf
 import numpy as np
+from dataset import Dataset
 
 # Just disables the warning, doesn't enable AVX/FMA cf
 # https://stackoverflow.com/questions/47068709/your-cpu-supports-instructions-that-this-tensorflow-binary-was-not
 # -compiled-to-u/50073238
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-# Data has to be preprocessed first !!
-def load_data():
-    image_index = []
-    with open('db/preprocessed/image.index', 'r') as f_handle:
-        formatted_image_index = f_handle.readlines()
-        for formatted_image_input in formatted_image_index:
-            image_index.append(formatted_image_input[:-1])
+class NeuralNetwork:
+    def __init__(self, gpu_check=True):
+        self.nothing = None
+        self.dataset = Dataset()
 
-    age_index = []
-    with open('db/preprocessed/age.index', 'r') as f_handle:
-        formatted_age_index = f_handle.readlines()
-        for formatted_age_input in formatted_age_index:
-            values = formatted_age_input[:-1]
-            values = values.split(" ")
+        if gpu_check:  # Check that GPU is correctly detected
+            tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-            age_index.append((float(values[0]), float(values[1])))
+        # Hyperparameters
+        self.n_inputs = 28 * 284
+        self.n_hidden_1 = 3005
+        self.n_hidden_2 = 1006
+        self.n_outputs = 1078
+        self.X = tf.placeholder(tf.float32, shape(None, self.n_inputs),
+                                name="X")  # input layer
+        self.y = tf.placeholder(tf.int64, shape(None),
+                                name="y")  # output layer
 
-    gender_index = []
-    with open('db/preprocessed/gender.index', 'r') as f_handle:
-        formatted_gender_index = f_handle.readlines()
-        for formatted_gender_input in formatted_gender_index:
-            gender_index.append(int(formatted_gender_input[:-1]))
+        # Define network model
+        with tf.name_scope("my_net"):
+            hidden_1 = tf.layers.dense(self.X, self.n_hidden_1, name="hidden_1", activation=tf.nn.relu)
+            hidden_2 = tf.layers.dense(hidden_1, self.n_hidden_2, name="hidden_2", activation=tf.nn.relu)
+            logits = tf.layers.dense(hidden_2, self.n_outputs, name="outputs")  # output before final activation function
 
-    train_count = 500
-    test_count = 50
+        # Define loss function
+        with tf.name_scope("loss"):
+            entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
 
-    x_train = []
-    y_train = []
-    x_test = []
-    y_test = []
+        # Define optimizer
+        with tf.name_scope("train"):
+            optimizer = tf.train.GradientDescentOptimizer(0.0001)
+            training_op = optimizer.minimize(loss)
 
-    for i in range(0, train_count):
-        x_train.append(cv2.imread(image_index[i]))
-        y_train.append(age_index[i])
+        # Define evaluation metric
+        with tf.name_scope("eval"):
+            correct = tf.nn.in_top_k(logits, y, 1)  # Doest the highest logit match with the class?
+            accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-    for i in range(train_count, test_count):
-        x_test.append(cv2.imread(image_index[i]))
-        y_test.append(age_index[i])
+    def train(self, batch_size, n_epochs):
+        # Train the network
+        with tf.Session as sess:
+            init.run()
+            for epoch in range(n_epochs):
+                for iteration in range(mnist.train.num_examples / batch_size):
+                    X_batch, y_batch = mnist.train.next_batch(batch_size)
+                    sess.run(training_op, feed_dict={self.X: X_batch, self.y: y_batch})
 
-    return (x_train, y_train), (x_test, y_test)
+                    training_acc = accuracy.eval(feed_dict={self.X: X_batch, self.y: y_batch})
+                    val_acc = accuracy.eval(feed_dict={self.X: mnist.validation_images, self.y: mnist_validation_labels})
 
-
-# Check that GPU is correctly detected
-tf.Session(config=tf.ConfigProto(log_device_placement=True))
-
-# Hyperparameters
-n_inputs = 28*284
-n_hidden_1 = 3005
-n_hidden_2 = 1006
-n_outputs = 1078
-X = tf.placeholder(tf.float32, shape(None, n_inputs),
-                   name="X")  # input layer
-y = tf.placeholder(tf.int64, shape(None),
-                   name="y")  # output layer
-
-# Define network model
-with tf.name_scope("my_net"):
-    hidden_1 = tf.layers.dense(X, n_hidden_1, name="hidden_1", activation=tf.nn.relu)
-    hidden_2 = tf.layers.dense(hidden_1, n_hidden_2, name="hidden_2", activation=tf.nn.relu)
-    logits = tf.layers.dense(hidden_2, n_outputs, name="outputs")  # output before final activation function
-
-# Define loss function
-with tf.name_scope("loss"):
-    entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
-
-# Define optimizer
-with tf.name_scope("train"):
-    optimizer = tf.train.GradientDescentOptimizer(0.0001)
-    training_op = optimizer.minimize(loss)
-
-# Define evaluation metric
-with tf.name_scope("eval"):
-    correct = tf.nn.in_top_k(logits, y, 1)  # Doest the highest logit match with the class?
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-
-# Training constants
-batch_size = 100
-n_epochs = 10
-
-# Train the network
-with tf.Session as sess:
-    init.run()
-    for epoch in range(n_epochs):
-        for iteration in range(mnist.train.num_examples / batch_size):
-            X_batch, y_batch = mnist.train.next_batch(batch_size)
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
-
-            training_acc = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
-            val_acc = accuracy.eval(feed_dict={X: mnist.validation_images, y: mnist_validation_labels})
-
-# Test the network
-with tf.Session as sess:
-    saver.restore(sess, "./my_path/model.ckpt")
-    Z = logits.eval(feed_dict={X: mnist.testing_images})
-    y_pred = np.argmax(Z, axis=1)
+    def test(self):
+        # Test the network
+        with tf.Session as sess:
+            saver.restore(sess, "./my_path/model.ckpt")
+            Z = logits.eval(feed_dict={self.X: mnist.testing_images})
+            y_pred = np.argmax(Z, axis=1)
