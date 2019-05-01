32b5f84831157ccd07cf300158e1cfcaf9c2ce72
Noémie <nlecocq@student.uliege.be>
2019-05-01 17:49:56 +0200

    Corrigé + pep8

--- dataset.py
+++ dataset.py
@@ -1,10 +1,14 @@
-import cv2
-import os
-import numpy as np
 import math
+import os
 import re
-import util
+
+import cv2
+import numpy as np
 import tensorflow as tf
+
+import util
+
+tf.enable_eager_execution()
 
 
 class Dataset:
@@ -15,14 +19,15 @@ class Dataset:
         self.image_index = []
         self.age_index = []
         self.gender_index = []
-        self.target_image_size = 200
+        self.target_image_size = 250
 
-        if not os.path.isfile(self.db_path + self.preprocessed_path + "lock") or force_preprocessing:
+        if not os.path.isfile(
+                self.db_path + self.preprocessed_path + "lock") or \
+                force_preprocessing:
             print("Started preprocessing data...")
             self.preprocess_data()
+            self.load_indexes()
             self.save_dataset()
-
-        self.load_indexes()
 
     def load_indexes(self):
         with open('db/preprocessed/image.index', 'r') as f_handle:
@@ -33,10 +38,10 @@ class Dataset:
         with open('db/preprocessed/age.index', 'r') as f_handle:
             formatted_age_index = f_handle.readlines()
             for formatted_age_input in formatted_age_index:
-                values = formatted_age_input[:-1]
-                values = values.split(" ")
-
-                self.age_index.append((float(values[0]), float(values[1])))
+                # values = formatted_age_input[:-1]
+                # values = values.split(" ")
+                self.age_index.append(formatted_age_input[:-1])
+                # self.age_index.append((float(values[0]), float(values[1])))
 
         with open('db/preprocessed/gender.index', 'r') as f_handle:
             formatted_gender_index = f_handle.readlines()
@@ -58,11 +63,13 @@ class Dataset:
         left, right = delta_w // 2, delta_w - (delta_w // 2)
 
         color = [0, 0, 0]
-        new_img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT,
+        new_img = cv2.copyMakeBorder(img, top, bottom, left, right,
+                                     cv2.BORDER_CONSTANT,
                                      value=color)
 
         # Ensure directory exists or is created !
-        save_path = self.db_path + self.preprocessed_path + img_path_in_db.replace("/", "_")
+        save_path = self.db_path + self.preprocessed_path + \
+                    img_path_in_db.replace("/", "_")
         directory = os.path.dirname(save_path)
         try:
             os.makedirs(directory)
@@ -73,9 +80,18 @@ class Dataset:
 
         return save_path
 
+    @staticmethod
+    def get_age_interval(mean):
+        age_intervals = [3, 7, 14, 24, 37, 47, 59, 150]
+        for i in range(len(age_intervals)):
+            if mean <= age_intervals[i]:
+                return i
+        return 3
+
     def preprocess_data(self):
         # Retrieve every image entry in the database
-        data_indexes = ["fold_0_data.txt", "fold_1_data.txt", "fold_2_data.txt", "fold_3_data.txt", "fold_4_data.txt"]
+        data_indexes = ["fold_0_data.txt", "fold_1_data.txt", "fold_2_data.txt",
+                        "fold_3_data.txt", "fold_4_data.txt"]
         entries = []
         for i in range(0, len(data_indexes)):
             index = open("db/" + data_indexes[i], 'r')
@@ -91,7 +107,9 @@ class Dataset:
             util.print_progress_bar(i + 1, len(entries), 'Resizing images : ')
             entry = entries[i]
             entry = entry.split("\t")
-            save_path = self.resize_image(entry[0] + "/" + "coarse_tilt_aligned_face." + entry[2] + "." + entry[1])
+            save_path = self.resize_image(
+                entry[0] + "/" + "coarse_tilt_aligned_face." + entry[2] + "." +
+                entry[1])
             image_index.append(save_path)
             try:
                 if entry[4] == "f" or entry[4] == "m":
@@ -102,10 +120,12 @@ class Dataset:
                     interval = re.sub("\(|\)|\s", "", entry[3]).split(",")
                     bottom = int(interval[0])
                     top = int(interval[1])
-                    age_index.append(((bottom + top) / 2, (top - bottom) / 2))
+                    mean = ((bottom + top) / 2, (top - bottom) / 2)
+                    age_index.append(self.get_age_interval(mean[0]))
                 else:
-                    age_index.append((int(entry[3]), 0))
-            except ValueError:  # ignore [incorrectly formatted / incomplete] lines
+                    age_index.append(self.get_age_interval(int(entry[3])))
+            except ValueError:  # ignore [incorrectly formatted / incomplete]
+                # lines
                 image_index.pop()
 
         # Flushing indexes to disk
@@ -115,36 +135,45 @@ class Dataset:
 
         with open('db/preprocessed/age.index', 'w+') as f_handle:
             for item in age_index:
-                f_handle.write('%s %s\n' % item)
+                f_handle.write('%s\n' % item)
 
         with open('db/preprocessed/gender.index', 'w+') as f_handle:
             for item in gender_index:
                 f_handle.write('%s\n' % item)
 
         # Work out mean image
-        mean_img = np.zeros((self.target_image_size, self.target_image_size, 3), np.uint32)
+        mean_img = np.zeros((self.target_image_size, self.target_image_size, 3),
+                            np.uint32)
         for i in range(0, len(image_index)):
-            util.print_progress_bar(i + 1, len(image_index), 'Working out mean : ')
+            util.print_progress_bar(i + 1, len(image_index),
+                                    'Working out mean : ')
             img = cv2.imread(image_index[i])
             mean_img += img
         mean_img = np.true_divide(mean_img, len(image_index))
         cv2.imwrite("db/preprocessed/mean.jpg", mean_img)
 
         # Work out standard deviation image
-        standard_deviation_img = np.zeros((self.target_image_size, self.target_image_size, 3), np.uint32)
+        standard_deviation_img = np.zeros(
+            (self.target_image_size, self.target_image_size, 3), np.uint32)
         for i in range(0, len(image_index)):
-            util.print_progress_bar(i + 1, len(image_index), 'Working out standard deviation : ')
+            util.print_progress_bar(i + 1, len(image_index),
+                                    'Working out standard deviation : ')
             img = cv2.imread(image_index[i])
-            standard_deviation_img += np.power(img - mean_img, 2).astype(np.uint32)
-        standard_deviation_img = np.sqrt(np.true_divide(standard_deviation_img, len(image_index) - 1))
-        cv2.imwrite("db/preprocessed/standard_deviation.jpg", standard_deviation_img)
+            standard_deviation_img += np.power(img - mean_img, 2).astype(
+                np.uint32)
+        standard_deviation_img = np.sqrt(
+            np.true_divide(standard_deviation_img, len(image_index) - 1))
+        cv2.imwrite("db/preprocessed/standard_deviation.jpg",
+                    standard_deviation_img)
 
         # Normalizing images
         absolute_min = math.inf
         absolute_max = -math.inf
         for i in range(0, len(image_index)):
-            util.print_progress_bar(i + 1, len(image_index), 'Retrieving image scale : ')
-            img = ((cv2.imread(image_index[i]).astype(np.int16) - mean_img) / standard_deviation_img)
+            util.print_progress_bar(i + 1, len(image_index),
+                                    'Retrieving image scale : ')
+            img = ((cv2.imread(image_index[i]).astype(
+                np.int16) - mean_img) / standard_deviation_img)
             img_min = np.amin(img)
             if img_min < absolute_min:
                 absolute_min = img_min
@@ -154,7 +183,8 @@ class Dataset:
             np.save(image_index[i] + ".npy", img.astype(np.int16))
 
         for i in range(0, len(image_index)):
-            util.print_progress_bar(i + 1, len(image_index), 'Normalizing images : ')
+            util.print_progress_bar(i + 1, len(image_index),
+                                    'Normalizing images : ')
             img = np.load(image_index[i] + ".npy").astype(np.float64)
             img -= absolute_min  # Start @ 0
             img *= 255 / (absolute_max - absolute_min)  # Occupy full scale
@@ -174,8 +204,11 @@ class Dataset:
         return image
 
     def save_dataset(self):
-        image_ds = tf.data.Dataset.from_tensor_slices(self.image_index).map(tf.read_file)
-        tfrec = tf.data.experimental.TFRecordWriter(self.db_path + 'images.tfrec')
+        # path_ds = tf.data.Dataset.from_tensor_slices(self.image_index)
+        image_ds = tf.data.Dataset.from_tensor_slices(self.image_index).map(
+            tf.read_file)
+        tfrec = tf.data.experimental.TFRecordWriter(
+            self.db_path + 'images.tfrec')
         tfrec.write(image_ds)
 
     def get_dataset(self):
@@ -185,16 +218,18 @@ class Dataset:
         train_size = int(0.7 * DATASET_SIZE)
         test_size = int(0.15 * DATASET_SIZE)
 
-        ds = tf.data.TFRecordDataset(self.db_path + 'images.tfrec').map(self.preprocess_image)
+        ds = tf.data.TFRecordDataset(self.db_path + 'images.tfrec').map(
+            self.preprocess_image)
         out_ds = tf.data.Dataset.from_tensor_slices(self.age_index)
         ds = tf.data.Dataset.zip((ds, out_ds))
 
-        # Setting a shuffle buffer size as large as the dataset ensures that the data is
-        # completely shuffled.
+        # Setting a shuffle buffer size as large as the dataset ensures that
+        # the data is completely shuffled.
         ds = ds.shuffle(buffer_size=DATASET_SIZE)
         ds = ds.repeat()
         ds = ds.batch(BATCH_SIZE)
-        # `prefetch` lets the dataset fetch batches, in the background while the model is training.
+        # `prefetch` lets the dataset fetch batches, in the background while
+        # the model is training.
         ds = ds.prefetch(buffer_size=AUTOTUNE)
 
         train_dataset = ds.take(train_size)
