90852470e7eb3149ad951bfc8840a6abded42aeb
Noémie <nlecocq@student.uliege.be>
2019-05-01 23:42:43 +0200

    pep8

--- network.py
+++ network.py
@@ -9,7 +9,10 @@ def network_def(input_layer, mode):
     iterative_layer = input_layer
 
     # Convolutional blocks
-    for filter_num, kernel_size, pool_size, dropout_rate in zip(filters, kernel_sizes, pool_sizes, dropout_rates):
+    for filter_num, kernel_size, pool_size, dropout_rate in zip(filters,
+                                                                kernel_sizes,
+                                                                pool_sizes,
+                                                                dropout_rates):
         cv_layer = tf.layers.conv2d(
             inputs=iterative_layer,
             filters=filters,
@@ -17,16 +20,21 @@ def network_def(input_layer, mode):
             padding="same",
             activation=tf.nn.relu)
 
-        pool = tf.layers.max_pooling2d(inputs=cv_layer, pool_size=[pool_size, pool_size], strides=2)
+        pool = tf.layers.max_pooling2d(inputs=cv_layer,
+                                       pool_size=[pool_size, pool_size],
+                                       strides=2)
 
         iterative_layer = tf.layers.dropout(
-            inputs=pool, rate=dropout_rate, training=mode == tf.estimator.ModeKeys.TRAIN)
+            inputs=pool, rate=dropout_rate,
+            training=mode == tf.estimator.ModeKeys.TRAIN)
 
     # Dense Layer
     flattened_pool = tf.layers.flatten(iterative_layer)
-    dense_layer_common = tf.layers.dense(inputs=flattened_pool, units=1024, activation=tf.nn.relu)
+    dense_layer_common = tf.layers.dense(inputs=flattened_pool, units=1024,
+                                         activation=tf.nn.relu)
     dropout_layer = tf.layers.dropout(
-        inputs=dense_layer_common, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+        inputs=dense_layer_common, rate=0.4,
+        training=mode == tf.estimator.ModeKeys.TRAIN)
 
     # Gender subnetwork
     gender_dense_layer = tf.layers.dense(inputs=dropout_layer, units=1024)
@@ -37,6 +45,3 @@ def network_def(input_layer, mode):
     age_output = tf.layers.dense(inputs=age_dense_layer, units=8)
 
     return age_output, gender_output
-
-
-
